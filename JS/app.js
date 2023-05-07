e7eb4db53f657938ba91bf530ca0589cd8052815
Nuria Oliveira <nuriaeo@gmail.com>
2023-05-07 01:55:27 -0300

    base del proyecto

---
+++ JS/app.js
@@ -0,0 +1,38 @@
+const productos = [
+    {id:1, categoria:"STAR WARS", descripcion: "MEGA DATH VADER", precio: 100, stock: 50},
+    {id:2, categoria:"STAR WARS", descripcion: "LUKE SKYWALKER", precio: 12, stock: 50},
+    {id:3, categoria:"STAR WARS", descripcion: "WICKET", precio: 12, stock: 50},
+    {id:4, categoria:"STAR WARS", descripcion: "C-3PO", precio: 12, stock: 50},
+    {id:5, categoria:"STAR WARS", descripcion: "CHEWBACCA", precio: 15, stock: 50},
+    {id:6, categoria:"STAR WARS", descripcion: "PRINCESS LEIA", precio: 12, stock: 50},
+    {id:7, categoria:"STAR WARS", descripcion: "R2-D2", precio: 15, stock: 50},
+    {id:8, categoria: "HARRY POTTER", descripcion: "HEDWING", precio: 12, stock: 50},
+    {id:9, categoria: "HARRY POTTER", descripcion: "HERMAIONE", precio: 12, stock: 50},
+    {id:10, categoria: "HARRY POTTER", descripcion: "DOBBY", precio: 12, stock: 50},
+    {id:11, categoria: "HARRY POTTER", descripcion: "HARRY POTTER", precio:12, stock: 50},
+    {id:12, categoria: "HARRY POTTER", descripcion: "SEVERYS SNAPE", precio:12, stock: 50},
+    {id:13, categoria: "HARRY POTTER", descripcion: "ALBUS DUMBLEDORE", precio:12, stock: 50},
+    {id:14, categoria: "HARRY POTTER", descripcion: "RON WASLEY", precio: 12, stock: 50},
+    {id:15, categoria: "PIXAR", descripcion: "JUMBO WALL-E", precio: 40, stock: 50},
+    {id:16, categoria: "PIXAR", descripcion: "SOX", precio: 12, stock: 50},
+    {id:17, categoria: "PIXAR", descripcion: "BOO", precio: 12, stock: 50},
+    {id:18, categoria: "PIXAR", descripcion: "BUZZ LIGHTYEAR", precio: 15, stock: 50},
+];
+
+
+let lista_productos = document.getElementById("lista_productos")
+
+productos.forEach((producto)=>{
+    
+    let funko = document.createElement("div");
+    funko.className = "col";
+    funko.innerHTML = `         <div class="card">
+                                    <img src="./img/${producto.id}.png" class="card-img-top" alt="funko">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${producto.descripcion}</h5>
+                                        <h6>${producto.categoria}</h6>
+                                        <p class="card-text">Precio: U$S ${producto.precio}</p>
+                                    </div>
+                                </div>`;
+    lista_productos.append(funko);
+});
